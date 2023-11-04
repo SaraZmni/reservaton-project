998f85f992855a0359d752ec7851c5094e8d71bd
szamani <szamani240@gmail.com>
2023-11-04 17:16:56 +0330

    delete material ui and add new pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const App: FC = () => {
-  return <>Hello world</>;
+  return <div>Hello world</div>;
 };
 
 export default App;
--- src/components/auth/auth.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-
-const LoginForm: FC = () => {
-  return <div>Hi</div>;
-};
-export default LoginForm;
